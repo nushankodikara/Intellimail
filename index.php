6b86ae52551048dffb8a52bead8c2a7ba35118ba
Nushan Kodikara <nushankodi@gmail.com>
2023-08-08 08:37:55 +0530

    Initialized Tailwind CSS

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Intelli Mail</title>
+    <?php include 'components/head.php'; ?>
+</head>
+
+<body>
+    <h1>Hello World!</h1>
+</body>
+
+</html>
