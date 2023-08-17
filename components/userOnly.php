819367eba3e7f5cee4189211b0e141cf7c28f6c5
nushankodikara <nushankodi@gmail.com>
2023-08-17 09:01:59 +0530

    bug fix

--- components/userOnly.php
+++ components/userOnly.php
@@ -10,7 +10,7 @@ $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
 
 if ($row['role'] !== 'user') {
-    header('Location: ../login.php');
+    header('Location: ../functions/logout.php');
 }
 
 ?>
