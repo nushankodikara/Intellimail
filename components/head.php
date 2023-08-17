ae1e1d8e4ba1e823460972111849ba84e9dcef94
Nushan Kodikara <nushankodi@gmail.com>
2023-08-17 07:32:37 +0530

    Added Favicons

--- components/head.php
+++ components/head.php
@@ -9,6 +9,14 @@
 <script
     src="https://www.paypal.com/sdk/js?client-id=AfOipv9PnIcc39FUscFViBHuEXHO8AhIViR6VVO5pOgyyOsOdraMhVQjIjSIn4w-JOlu5nLC-KOQfTOM"></script>
 
+<!-- Favicons -->
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+
 <script>
     tailwind.config = {
 
